f2471508038cd024528e7c97460a676cbb943b70
Alex <darksunhd@gmail.com>
2017-10-29 00:48:25 +0200

    change Makefile (now very simple)

--- irc/src/irc_main.cpp
+++ irc/src/irc_main.cpp
@@ -3,7 +3,14 @@
 
 using namespace std;
 
-int main() {
-   cout << "Hello World";
+int main(int argc, char* argv[]) {
+        if(argc < 3){
+                std:cout << "missing start parameter\n";
+                return 0;
+        }
+
+        if(std::string(argv[1]) == "-s"){
+                cout << std::string(argv[2]);
+        }
    return 0;
 }
